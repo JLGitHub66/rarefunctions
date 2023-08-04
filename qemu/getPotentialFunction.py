90e46b2c7e8b0f346953368bb5b793d1177c8e20
lei <njl18810931102@gmail.com>
2023-08-04 21:52:11 +0800

    qemu result

---
+++ qemu/getPotentialFunction.py
@@ -0,0 +1,26 @@
+import argparse
+import os
+
+'''
+Usage:
+    python3 RankingAndSim.py api_sequences.txt
+    api_sequences.txt: the file containing the API sequences.
+    ft.vec: the fastText model trained from the api_sequences.
+'''
+parser = argparse.ArgumentParser(description='get single free funcs')
+parser.add_argument('rareFuncFile', metavar='known', type=str, 
+                    help='The file containing rare function)')
+
+args = parser.parse_args()
+
+fRareFunc = args.rareFuncFile
+
+print("loading rare function file: {}".format(os.path.abspath(fRareFunc)))
+frare = open(fRareFunc, "r")
+rare_funcs = frare.read().split('\n')
+frare.close()
+
+for rare_func in rare_funcs:
+    #if "alloc" in rare_func or "_init" in rare_func or "init_" in rare_func  or "_new" in rare_func or "new_" in rare_func:
+    if "free" in rare_func or "release" in rare_func or "destory" in rare_func:
+        print(rare_func)
