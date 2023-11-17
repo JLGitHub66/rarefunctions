858835a7b2e32b040a62770dc71b8b7337cb7867
lei <njl18810931102@gmail.com>
2023-11-17 17:45:45 +0800

    ffmpeg-6.0 experiment data

--- ffmpeg-6.0/getPotentialFunction.py
+++ ffmpeg-6.0/getPotentialFunction.py
@@ -21,6 +21,9 @@ rare_funcs = frare.read().split('\n')
 frare.close()
 
 for rare_func in rare_funcs:
-    #if "alloc" in rare_func or "new" in rare_func:
-    if "free" in rare_func or "release" in rare_func or "destory" in rare_func:
+    if "alloc" in rare_func:
+    #if "free" in rare_func or "release" in rare_func or "destory" in rare_func:
+    #if rare_func.startswith("lock_") or rare_func.endswith("_lock") or "_lock_" in rare_func:
+    #if rare_func.startswith("unlock_") or rare_func.endswith("_unlock") or "_unlock_" in rare_func:
+    #if "printf" in rare_func:
         print(rare_func)
